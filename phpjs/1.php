2d75c9f2b941a24ff2279758c24db67f6197384d
paypulse <sourcedev5@gmail.com>
2017-10-19 20:25:00 +0900

    Add javascript Hello World [ProgrammingTest]

--- phpjs/1.php
+++ phpjs/1.php
@@ -1,1 +1,24 @@
-Hello world
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset ="utf-8" />
+
+  </head>
+  <body>
+
+    <h1>php</h1>
+    <!--지금 부터 php가 시작된다는것을 php 엔진에게 갈려주는 시작/끝을 의미하는 약속 기호--->
+    <?php
+      echo "Hello world";
+      echo 10 + 10;
+    ?>
+    
+    <!--- javascript ------>
+    <h1>Javascript</h1>
+    <script type="text/javascript">
+      document.writeln("Hello world");
+      document.write(10+10);
+    </script>
+
+  </body>
+</html>
