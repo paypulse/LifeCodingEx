f8fdafeed18dcccd87073cee36a0e469c142b735
paypulse <sourcedev5@gmail.com>
2017-10-29 19:38:14 +0900

    javascriptEx02

--- javascript/scriptEx.js
+++ javascript/scriptEx.js
@@ -3,6 +3,7 @@
 //document.write(5+6);
 
 /*바탕 화면 바꾸기*/
+
 function blackClick() {
   document.getElementById("target").className ="white";
 }
@@ -10,3 +11,16 @@ function blackClick() {
 function whiteClick() {
   document.getElementById("target").className ="black";
 }
+
+
+/*id 로 접근해보자*/
+var whiteBtn = document.getElementById("whiteBtn");
+var blackBtn = document.getElementById("blackBtn");
+
+whiteBtn.addEventListener('click', function(){
+  document.getElementById("target").className ="black";
+})
+
+blackBtn.addEventListener('click', function(){
+  document.getElementById("target").className="white";
+})
