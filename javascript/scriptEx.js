4091ffc75795b34a35f59d7b7274d4d7e3a0cabe
paypulse <sourcedev5@gmail.com>
2017-10-29 19:28:54 +0900

    Javascript Ex01

---
+++ javascript/scriptEx.js
@@ -0,0 +1,12 @@
+
+//console.log("Test 123");
+//document.write(5+6);
+
+/*바탕 화면 바꾸기*/
+function blackClick() {
+  document.getElementById("target").className ="white";
+}
+
+function whiteClick() {
+  document.getElementById("target").className ="black";
+}
